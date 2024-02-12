0c00966cd7acf837869fdca0ebf8365323438afb
Candy <obiijeoma32@gmail.com>
2024-02-12 17:25:55 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
+import { useRef } from "react";
 import AboutMe from "./Components/AboutMe";
-import Header from "./Components/Header";
 import HeroSection from "./Components/HeroSection";
 import Projects from "./Components/Projects";
 import Skills from "./Components/Skills";
 import Resume from "./Components/Resume";
 import Certification from "./Components/Certification";
 import Contact from "./Components/Contact";
+import Header from "./Components/Header";
 import "./App.css";
 
 function App() {
+  const heroSection = useRef(null);
+  const about = useRef(null);
+  const project = useRef(null);
+  const skill = useRef(null);
+  const resume = useRef(null);
+  const certification = useRef(null);
+  const contact = useRef(null);
+
+  const scrollToSection = (elementRef) => {
+    window.scrollTo({
+      top: elementRef.current.offsetTop,
+      behavior: "smooth",
+    });
+  };
+
   return (
     <>
-      <Header />
-      <HeroSection />
-      <AboutMe />
-      <Projects />
-      <Skills />
-      <Resume />
-      <Certification />
-      <Contact />
+      <Header resume={resume} certification={certification} contact={contact} scrollToSection={scrollToSection} heroSection={heroSection} about={about} project={project} skill={skill} />
+      <div ref={heroSection}>
+        <HeroSection />
+      </div>
+      <div ref={about}>
+        <AboutMe />
+      </div>
+      <div ref={project}>
+        <Projects />
+      </div>
+      <div ref={skill}>
+        <Skills />
+      </div>
+      <div ref={resume}>
+        <Resume />
+      </div>
+      <div ref={certification}>
+        <Certification />
+      </div>
+      <div ref={contact}>
+        <Contact />
+      </div>
     </>
   );
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import logo from "./Images/Logo.svg";
-function Header() {
+function Header({ contact, skill, resume, scrollToSection, heroSection, project, about }) {
   const [hamburger, setHamburger] = useState(false);
   const [navBar, setNavBar] = useState(false);
   function handleScrollBackground() {
@@ -16,26 +16,49 @@ function Header() {
     <>
       {/* BIG SCREENS */}
       <div className={`100:hidden xl:flex z-20 w-full  flex justify-center items-center h-[99px] px-[60px] py-[33px] fixed ${navBar ? "bg-[#007BFF] bg-opacity-30" : "bg-transparent"} `}>
-        <div className=" xl:w-full 2xl:max-w-[1400px]  justify-between items-center inline-flex">
+        <div className=" xl:w-[85%] 2xl:max-w-[1400px]  justify-between items-center inline-flex">
           <div className="text-zinc-800 flex justify-between items-center gap-[60px] text-lg font-semibold font-montserrat tracking-tight">
-            <h3>About Me</h3>
-            <h3>Projects</h3>
-            <h3>Resume</h3>
-            <h3>Home</h3>
+            <h3 className=" cursor-pointer " onClick={() => scrollToSection(about)}>
+              About Me
+            </h3>
+            <h3 className=" cursor-pointer " onClick={() => scrollToSection(project)}>
+              Projects
+            </h3>
+            <h3 className=" cursor-pointer " onClick={() => scrollToSection(heroSection)}>
+              Home
+            </h3>
           </div>
-          <img src={logo} alt="immaculate" />
+          <img
+            className=" cursor-pointer"
+            onClick={() => {
+              scrollToSection(heroSection);
+            }}
+            src={logo}
+            alt="immaculate"
+          />
           <div className="text-zinc-800 flex justify-between items-center gap-[60px]  text-lg font-semibold font-montserrat tracking-tight">
-            <h3>Skills</h3>
+            <h3 className=" cursor-pointer " onClick={() => scrollToSection(resume)}>
+              Resume
+            </h3>
+            <h3 className=" cursor-pointer " onClick={() => scrollToSection(skill)}>
+              Skills
+            </h3>
 
-            <h3>Experience</h3>
-            <h3>Education</h3>
-            <h3>Contact</h3>
+            <h3 className=" cursor-pointer " onClick={() => scrollToSection(contact)}>
+              Contact
+            </h3>
           </div>
         </div>
       </div>
       {/* SMALL SCREENS FROM 100px TO 1279px */}
       <div className={`100:flex xl:hidden z-20 w-full  flex justify-between items-center h-[99px] px-[60px] py-[33px] fixed ${navBar ? "bg-[#007BFF] bg-opacity-30" : "bg-transparent"} `}>
-        <img src={logo} alt="immaculate" />
+        <img
+          onClick={() => {
+            scrollToSection(heroSection);
+          }}
+          src={logo}
+          alt="immaculate"
+        />
         <svg onClick={() => setHamburger(true)} width="30" height="31" viewBox="0 0 30 31" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M25 15.5H12.5" stroke="#333333" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
           <path d="M25 6.75H5" stroke="#333333" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
@@ -48,10 +71,61 @@ function Header() {
             <path d="M19 5L5 19M5 5L19 19" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
           </svg>
           <div className="text-white flex flex-col mt-[250px] justify-between items-center gap-[60px]  text-lg font-semibold font-montserrat tracking-tight">
-            <h3>Skills</h3>
-            <h3>Experience</h3>
-            <h3>Education</h3>
-            <h3>Contact</h3>
+            <h3
+              className=" cursor-pointer "
+              onClick={() => {
+                scrollToSection(about);
+                setHamburger(false);
+              }}
+            >
+              About Me
+            </h3>
+            <h3
+              className=" cursor-pointer "
+              onClick={() => {
+                scrollToSection(project);
+                setHamburger(false);
+              }}
+            >
+              Projects
+            </h3>
+            <h3
+              className=" cursor-pointer "
+              onClick={() => {
+                scrollToSection(heroSection);
+                setHamburger(false);
+              }}
+            >
+              Home
+            </h3>
+            <h3
+              className=" cursor-pointer "
+              onClick={() => {
+                scrollToSection(resume);
+                setHamburger(false);
+              }}
+            >
+              Resume
+            </h3>
+            <h3
+              className=" cursor-pointer "
+              onClick={() => {
+                scrollToSection(skill);
+                setHamburger(false);
+              }}
+            >
+              Skills
+            </h3>
+
+            <h3
+              className=" cursor-pointer "
+              onClick={() => {
+                scrollToSection(contact);
+                setHamburger(false);
+              }}
+            >
+              Contact
+            </h3>
           </div>
         </div>
       )}
